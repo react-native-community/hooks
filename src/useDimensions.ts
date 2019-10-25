f7647c2bc34446b960453ec9441a79223aedd4ec
Pavlos Vinieratos <pvinis@gmail.com>
2019-10-25 08:13:53 +0200

    just rename

---
+++ src/useDimensions.ts
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react'
+import { Dimensions } from 'react-native'
+
+const window = Dimensions.get('window')
+const screen = Dimensions.get('screen')
+
+export default () => {
+  const [dimensions, setDimensions] = useState({
+    window, screen
+  })
+
+  onChange = ({ window, screen }) => {
+    setDimensions({ window, screen })
+  }
+
+  useEffect(() => {
+    Dimensions.addEventListener('change', onChange)
+
+    return () => Dimensions.removeEventListener('change', onChange)
+  }, [])
+
+  return dimensions
+}
---
+++ src/useInteractionManager.ts
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { InteractionManager } from 'react-native';
+
+export default () => {
+  const [complete, updateInteractionStatus] = useState(false);
+
+  useEffect(() => {
+    InteractionManager.runAfterInteractions(() => {
+      updateInteractionStatus(true)
+    })
+  }, [])
+  return complete
+};
