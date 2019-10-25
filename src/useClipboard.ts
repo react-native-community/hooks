6643667acc46624685b0e34d5bda04285752eece
Pavlos Vinieratos <pvinis@gmail.com>
2019-10-25 08:12:42 +0200

    clipboard

---
+++ src/useClipboard.ts
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import { Clipboard } from 'react-native'
+
+export default () => {
+  const [data, updateClipboardData] = useState('')
+
+  useEffect(() => {
+	  (async () => {
+		  const content = await Clipboard.getString()
+		  updateClipboardData(content)
+	  })()
+  }, [])
+
+  function setString(content: string) {
+    Clipboard.setString(content)
+    updateClipboardData(content)
+  }
+
+  return [data, setString]
+}
