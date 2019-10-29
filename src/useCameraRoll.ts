d34bdf06e11bdf9ff3de22e992d964dfc2f44e3f
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-10-30 00:18:13 +0900

    feat: add types for useCameraRoll

--- src/useCameraRoll.ts
+++ src/useCameraRoll.ts
@@ -30,7 +30,7 @@ export default function() {
     }
   }
 
-  async function saveToCameraRoll(tag, type) {
+  async function saveToCameraRoll(tag:string, type:'photo'|'video'|undefined) {
     try {
       await CameraRoll.saveToCameraRoll(tag, type)
     } catch (err) {
