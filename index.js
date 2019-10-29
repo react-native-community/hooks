632932338e1447423b23aa35fcfc7bb509a0c940
Jesse Katsumata <jesse.katsumata@gmail.com>
2019-10-30 00:27:49 +0900

    fix: remove deprecated api

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-import useDimensions from './lib/useDimensions'
-import useAppState from './lib/useAppState'
-import useCameraRoll from './lib/useCameraRoll'
-import useClipboard from './lib/useClipboard'
-import useAccessibilityInfo from './lib/useAccessibilityInfo'
-import useGeolocation from './lib/useGeolocation'
-import useNetInfo from './lib/useNetInfo'
-import useKeyboard from './lib/useKeyboard'
-import useInteractionManager from './lib/useInteractionManager'
-import useDeviceOrientation from './lib/useDeviceOrientation'
+import useDimensions from "./lib/useDimensions";
+import useAppState from "./lib/useAppState";
+import useCameraRoll from "./lib/useCameraRoll";
+import useClipboard from "./lib/useClipboard";
+import useAccessibilityInfo from "./lib/useAccessibilityInfo";
+import useKeyboard from "./lib/useKeyboard";
+import useInteractionManager from "./lib/useInteractionManager";
+import useDeviceOrientation from "./lib/useDeviceOrientation";
 
 export {
   useDimensions,
@@ -15,9 +13,7 @@ export {
   useCameraRoll,
   useClipboard,
   useAccessibilityInfo,
-  useGeolocation,
-  useNetInfo,
   useKeyboard,
   useInteractionManager,
-  useDeviceOrientation,
-}
+  useDeviceOrientation
+};
