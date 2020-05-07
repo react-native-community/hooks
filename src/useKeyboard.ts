b4fc1682004a11d819bf223d861ea0e7830f82e3
Ray Deck <ray@raydeck.com>
2020-05-06 22:07:41 -0500

    Adding hiding and showing state flags to useKeyboard

--- src/useKeyboard.ts
+++ src/useKeyboard.ts
@@ -14,6 +14,8 @@ const initialValue = {
 
 export function useKeyboard() {
   const [shown, setShown] = useState(false)
+  const [showing, setShowing] = useState(false)
+  const [hiding, setHiding] = useState(false)
   const [coordinates, setCoordinates] = useState<{
     start: ScreenRect
     end: ScreenRect
@@ -21,18 +23,22 @@ export function useKeyboard() {
   const [keyboardHeight, setKeyboardHeight] = useState<number>(0)
 
   const handleKeyboardWillShow: KeyboardEventListener = (e) => {
+    setShowing(true)
     setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
   }
   const handleKeyboardDidShow: KeyboardEventListener = (e) => {
     setShown(true)
+    setShowing(false)
     setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
     setKeyboardHeight(e.endCoordinates.height)
   }
   const handleKeyboardWillHide: KeyboardEventListener = (e) => {
+    setHiding(true)
     setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
   }
   const handleKeyboardDidHide: KeyboardEventListener = (e) => {
     setShown(false)
+    setHiding(false)
     if (e) {
       setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
     } else {
@@ -57,6 +63,8 @@ export function useKeyboard() {
 
   return {
     keyboardShown: shown,
+    keyboardShowing: showing,
+    keyboardHiding: hiding,
     coordinates,
     keyboardHeight,
   }
