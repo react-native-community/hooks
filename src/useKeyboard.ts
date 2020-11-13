05cfdc21a233b571ffa8148d201d2c0f7ef641e3
Justin Makaila <justin@wellthapp.com>
2020-11-13 13:08:40 -0500

    add support for keyboard animation meta

--- src/useKeyboard.ts
+++ src/useKeyboard.ts
@@ -1,42 +1,63 @@
 import {useEffect, useState} from 'react'
 import {Keyboard, KeyboardEventListener, ScreenRect} from 'react-native'
 
+export type KeyboardAnimationType = 'show' | 'hide'
+interface KeyboardAnimationEvent {
+  active: boolean
+  duration: number
+  type: KeyboardAnimationType
+}
+
 const emptyCoordinates = Object.freeze({
   screenX: 0,
   screenY: 0,
   width: 0,
   height: 0,
 })
-const initialValue = {
+const initialCoordinates = {
   start: emptyCoordinates,
   end: emptyCoordinates,
 }
 
+const initialAnimation: KeyboardAnimationEvent = {
+  active: false,
+  duration: 0,
+  type: 'show',
+}
+
 export function useKeyboard() {
   const [shown, setShown] = useState(false)
+  const [animation, setAnimation] = useState<KeyboardAnimationEvent>(
+    initialAnimation,
+  )
   const [coordinates, setCoordinates] = useState<{
     start: ScreenRect
     end: ScreenRect
-  }>(initialValue)
+  }>(initialCoordinates)
   const [keyboardHeight, setKeyboardHeight] = useState<number>(0)
 
   const handleKeyboardWillShow: KeyboardEventListener = (e) => {
+    setAnimation({active: true, duration: e.duration, type: 'show'})
     setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
   }
   const handleKeyboardDidShow: KeyboardEventListener = (e) => {
     setShown(true)
+    setAnimation({active: false, duration: e.duration, type: 'show'})
     setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
     setKeyboardHeight(e.endCoordinates.height)
   }
   const handleKeyboardWillHide: KeyboardEventListener = (e) => {
+    setAnimation({active: true, duration: e.duration, type: 'hide'})
     setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
   }
   const handleKeyboardDidHide: KeyboardEventListener = (e) => {
     setShown(false)
     if (e) {
+      setAnimation({active: false, duration: e.duration, type: 'hide'})
       setCoordinates({start: e.startCoordinates, end: e.endCoordinates})
     } else {
-      setCoordinates(initialValue)
+      setAnimation(initialAnimation)
+      setCoordinates(initialCoordinates)
       setKeyboardHeight(0)
     }
   }
@@ -58,6 +79,7 @@ export function useKeyboard() {
   return {
     keyboardShown: shown,
     coordinates,
+    animation,
     keyboardHeight,
   }
 }
