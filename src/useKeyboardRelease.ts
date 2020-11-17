4992d57fc46bccb8d928281b03a2b24150664a3f
Justin Noel <justin.noel@calendee.com>
2020-11-17 06:57:58 -0600

    feat: add useKeyboardRelease hook
    Allows user to easily dismiss the keyboard inside React Native views.
    
    https://justinnoel.dev/2020/11/17/react-native-dismiss-keyboard-custom-hook/

---
+++ src/useKeyboardRelease.ts
@@ -0,0 +1,13 @@
+import { Keyboard } from 'react-native'
+
+export interface KeyboardReleaseReturns {
+  shouldSetResponse: () => boolean
+  onRelease: () => void
+}
+
+export function useKeyboardRelease(): KeyboardReleaseReturns {
+  const shouldSetResponse = () => true
+  const onRelease = () => Keyboard.dismiss()
+
+  return { onRelease, shouldSetResponse }
+}
