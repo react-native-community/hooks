dc0c6a43b60b76f3ec7047a975f1a3b3f5987a74
David <4661784+retyui@users.noreply.github.com>
2021-10-14 20:12:07 +0300

    Add simple tests & update dev dependencies

---
+++ src/useInteractionManager.test.ts
@@ -0,0 +1,38 @@
+import {useInteractionManager} from './useInteractionManager'
+import {act, renderHook} from '@testing-library/react-hooks'
+import {InteractionManager} from 'react-native'
+
+jest.mock('react-native', () => ({
+  InteractionManager: {
+    runAfterInteractions: jest.fn(),
+  },
+}))
+
+describe('useInteractionManager', () => {
+  const runAfterInteractionsMock =
+    InteractionManager.runAfterInteractions as jest.Mock
+
+  it('should return false by default', () => {
+    const {result} = renderHook(() => useInteractionManager())
+
+    expect(result.current).toBe(false)
+  })
+
+  it('should return true after all interactions have completed', () => {
+    let emitAfterInteractions = () => {}
+
+    runAfterInteractionsMock.mockImplementationOnce((cb) => {
+      emitAfterInteractions = cb
+    })
+
+    const {result} = renderHook(() => useInteractionManager())
+
+    expect(result.current).toBe(false)
+
+    act(() => {
+      emitAfterInteractions()
+    })
+
+    expect(result.current).toBe(true)
+  })
+})
