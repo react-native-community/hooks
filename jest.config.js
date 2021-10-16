1edfd614ec10ab26ba988cea1ab69bd2a3ae81ba
David <4661784+retyui@users.noreply.github.com>
2021-10-16 20:51:00 +0300

    Add useKeyboard tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  preset: 'ts-jest',
+  testEnvironment: 'node',
+  preset: 'react-native',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  transform: {
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testPathIgnorePatterns: ['node_modules', 'lib'],
 }
