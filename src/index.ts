721c06aeefe8bb9ad85af9c5e927b9e11eb4c325
Babis Kanellopoulos <babis.kane@gmail.com>
2023-01-14 14:42:12 +0200

    chore: expose useRefresh

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {useInteractionManager} from './useInteractionManager'
 import {useDeviceOrientation} from './useDeviceOrientation'
 import {useLayout} from './useLayout'
 import {useImageDimensions} from './useImageDimensions'
+import {useRefresh} from './useRefresh'
 
 export {
   useDimensions,
@@ -23,4 +24,5 @@ export {
   useDeviceOrientation,
   useLayout,
   useImageDimensions,
+  useRefresh,
 }
